a2419715c66d4460708cd91e50318f7336b4e4c6
Márk Vince Varga <mark.vince.varga@gmail.com>
2025-05-02 16:21:17 +0200

    add new capability without changing capabilities.json
    This is done to demonstrate that the action fails if this happens

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
-	fmt.Println("Making a network call to example.org...")
 
-	// Make a network request to example.org
+	// CAPABILITY_NETWORK
 	_, err := http.Get("http://example.org")
 	if err != nil {
 		fmt.Printf("Error making HTTP request: %v\n", err)
-		os.Exit(1)
 	}
 
+	// CAPABILITY_READ_SYSTEM_STATE
+	cwd, _ := os.Getwd()
+	fmt.Println("Current working directory:", cwd)
 }
